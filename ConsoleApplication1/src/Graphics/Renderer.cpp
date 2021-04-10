ab67ac4e7b1065e9fc0290c689424cbe23db2fce
Hugo Juárez <aom921@hotmail.com>
2021-04-09 20:07:37 -0500

    Cambio de demo de OpenGL por visualizador primitivo.

--- ConsoleApplication1/src/Graphics/Renderer.cpp
+++ ConsoleApplication1/src/Graphics/Renderer.cpp
@@ -19,5 +19,17 @@ void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& 
 	shader.Bind();
 	va.Bind();
 	ib.Bind();
-    GlCall(glDrawElements(GL_TRIANGLES, ib.GetCount() , GL_UNSIGNED_INT, nullptr));
+	GlCall(glLineWidth(3.0));
+    GlCall(glDrawElements(GL_LINES_ADJACENCY, ib.GetCount() , GL_UNSIGNED_INT, nullptr));
+}
+
+void Renderer::DrawPoints(const VertexArray& va,const Shader& shader ,unsigned int first ) const
+{
+	shader.Bind();
+	va.Bind();
+	
+
+	unsigned int count = 2;
+	
+	GlCall(glDrawElements(GL_TRIANGLES, 2, GL_UNSIGNED_INT, nullptr));
 }
--- ConsoleApplication1/src/Graphics/VertexBufferLayout.h
+++ ConsoleApplication1/src/Graphics/VertexBufferLayout.h
@@ -18,7 +18,8 @@ struct VertexBufferElement
 		{
 		case GL_FLOAT: return 4;
 		case GL_UNSIGNED_INT: return 4;
-		case GL_UNSIGNED_BYTE: 1;
+		case GL_UNSIGNED_BYTE: return 1;
+		case GL_SHORT: return 2;
 		}
 		ASSERT(false);
 		return 0;
@@ -60,6 +61,13 @@ public:
 	{
 		m_Elements.push_back({ GL_UNSIGNED_BYTE,count, GL_TRUE });
 		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
+	}
+
+	template<>
+	void Push<short>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_SHORT,count,GL_TRUE });
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_SHORT);
 	}
 		
 
